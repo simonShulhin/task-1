e89b6c473c6d80ed16347952eb1c4377f3571103
Vladyslav <baskov228322@gmail.com>
2022-05-18 14:43:47 +0300

    matrix added

---
+++ matrix/index.js
@@ -0,0 +1,18 @@
+// a = 97 
+
+function grid(size) {
+    const res = []
+    for (let i = 0; i < size; i++) {
+        res[i] = [];
+        for (let j = 0; j < size; j++) {
+            res[i][j] = String.fromCharCode(97 + i + j);
+        }
+
+    }
+    return size < 0 ? null : res
+}
+// console.log(grid(5));
+// console.log(grid(6));
+// console.log(grid(-3));
+// console.log(grid(2));
+// console.log(grid(3.5));
