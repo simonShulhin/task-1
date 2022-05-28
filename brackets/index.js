aa64f319d95bf134a1dda91be10f834b003f3f62
simonShulhin <42201346+simonShulhin@users.noreply.github.com>
2022-05-28 10:29:17 +0300

    Bugs

--- brackets/index.js
+++ brackets/index.js
@@ -2,10 +2,12 @@ function isValidParentheses(string) {
     string = string.replaceAll(" ", '');
     const stack = [];
     for (let i = 0; i < string.length; i++) {
+        // не лишай консолі в коді
         console.log(stack, string[i]);
         if (string[i] === "(") {
             stack.push("(")
         } else {
+            // використовуй !== замість != задля уникнення помилок
             if (stack.length != 0) {
                 stack.pop();
             } else {
